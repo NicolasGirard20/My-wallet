01545c878a3b7f40742c3bd6440035355b09eb29
Nicolas Girard <francisco.girard@uap.edu.ar>
2026-08-13 12:25:06 -0300

    New changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,9 @@ import "dotenv/config"
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    seed: "pnpm prisma:seed",
+  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
